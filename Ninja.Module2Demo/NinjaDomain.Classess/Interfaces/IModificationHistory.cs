69ea34511f60184732e1c00e9731efd076d8ce9f
kelvin nguyen <kelvinguyen@hotmail.com>
2016-10-05 06:44:52 -0600

    add datahelper class to be able to dump data to database

---
+++ Ninja.Module2Demo/NinjaDomain.Classess/Interfaces/IModificationHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.Classess.Interfaces
+{
+    public interface IModificationHistory
+    {
+        DateTime DateModify { get; set; }
+        DateTime DateCreate { get; set; }
+        bool IsDirty { get; set; }
+
+    }
+}
---
+++ Ninja.Module2Demo/NinjaDomain.DataModel/DataHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NinjaDomain.Classess;
+
+namespace NinjaDomain.DataModel
+{
+    public class DataHelpers
+    {
+        public static void NewDBWithSeed()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<NinjaContext>());
+            using (var context = new NinjaContext())
+            {
+                if (context.Ninjas.Any())
+                    return;
+
+                var vtclas = context.Clans.Add(new Clan { ClanName = "White Clan" });
+            }
+        }
+    }
+}
