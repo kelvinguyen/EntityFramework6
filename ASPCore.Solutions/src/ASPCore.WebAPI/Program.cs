608c417dc537ce09cd99325d40a45091f11c1535
kelvin nguyen <knguyen2@mmm.com>
2016-11-15 10:40:37 -0700

    Initial Empty Project WebAPI in ASPCore.Solution

---
+++ ASPCore.Solutions/src/ASPCore.WebAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ASPCore.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()                     // initialize WebHost object
+                .UseKestrel()                                   // Krestrel : cross platform web server  vs WebListener : Window only webserver
+                .UseContentRoot(Directory.GetCurrentDirectory())// application base path = same as wwwroot path
+                .UseIISIntegration()                            // Use IIS as reverse proxy server for krestrel webserver
+                .UseStartup<Startup>()                          // startup type : is used by WebHost                          
+                .Build();                                       // Build() : return Instance of WebHost
+
+            host.Run();                                         // Run() : run the application
+        }
+    }
+}
---
+++ ASPCore.Solutions/src/ASPCore.WebAPI/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ASPCore.WebAPI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        /*
+         * ConfigureServices :  - Add services to the container
+         *                      - Container : is use for dependency injection
+         *                      - ConfigureServices() : is optional method
+         * 
+         * Services : - 
+         * */
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        /**
+         *  Configure :  - After ConfigureServices Method
+         *               - How asp.net core will response to HttpRequest
+         */
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
